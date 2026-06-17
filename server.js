409aa6fdb9581e188bed462980d28e962d03c8b1
Clara Fagundes <clara.nunes@edu.unimontes.br>
2026-06-17 10:02:28 -0300

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -46,4 +46,6 @@ app.delete('/filmes/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
